76cd1762c5e0580bb59be890ea3791c7914559a1
viditjoshi <viditjoshi94@gmail.com>
2024-10-06 19:15:46 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       "/api/v1": {
         target: "https://ecommerce-jewellery-site-backend.onrender.com", // Backend API server
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/v1/, "/api/v1"),
+        rewrite: (path) => path.replace(/^\/api\/v1/, "/api/v1/"),
       },
     },
   },
